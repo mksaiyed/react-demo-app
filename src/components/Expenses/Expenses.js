107ddeae8760aa8c3e639d06cc0cf5fb64d22e97
mksaiyed <mo.kaifbapu@gec28.ac.in>
2022-02-15 14:56:33 +0530

    Assignment - 2

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFiler from "./ExpensesFilter";
+
+function Expenses(props) {
+    const [SelectedYear, setSelectedYear] = useState("2021");
+
+    const selectedYearHandler = (year) => {
+        setSelectedYear(year);
+        console.log(year);
+    };
+
+    return (
+        <Card className="expenses">
+            <ExpensesFiler
+                selected={SelectedYear}
+                onSelectedYear={selectedYearHandler}
+            />
+            <ExpenseItem
+                title={props.data[0].title}
+                amount={props.data[0].amount}
+                date={props.data[0].date}
+            />
+            <ExpenseItem
+                title={props.data[1].title}
+                amount={props.data[1].amount}
+                date={props.data[1].date}
+            />
+            <ExpenseItem
+                title={props.data[2].title}
+                amount={props.data[2].amount}
+                date={props.data[2].date}
+            />
+            <ExpenseItem
+                title={props.data[3].title}
+                amount={props.data[3].amount}
+                date={props.data[3].date}
+            />
+        </Card>
+    );
+}
+
+export default Expenses;
